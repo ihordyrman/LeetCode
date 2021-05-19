578c2118602f979c17ed8a4a7c1824ca197a2116
nobodyshere <ihor.dyrman@outlook.com>
2021-05-19 07:55:16 +0300

    Add 1221

---
+++ 1221/Program.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace _1221
+{
+    /// <summary>
+    /// 1221. Split a String in Balanced Strings
+    /// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Assert.Equal(BalancedStringSplit("RLRRLLRLRL"), 4);
+            Assert.Equal(BalancedStringSplit("RLLLLRRRLR"), 3);
+            Assert.Equal(BalancedStringSplit("LLLLRRRR"), 1);
+            Assert.Equal(BalancedStringSplit("RLRRRLLRLL"), 2);
+        }
+
+        private static int BalancedStringSplit(string s)
+        {
+            var moveTo = 0;
+            var count = 0;
+            for (var i = 0; i < s.Length; i = moveTo)
+            {
+                var rCount = 0;
+                var lCount = 0;
+                for (var j = i; j < s.Length; j++)
+                {
+                    switch (s[j])
+                    {
+                        case 'R':
+                            rCount++;
+                            break;
+                        case 'L':
+                            lCount++;
+                            break;
+                    }
+
+                    if (lCount == rCount)
+                    {
+                        moveTo = ++j;
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
