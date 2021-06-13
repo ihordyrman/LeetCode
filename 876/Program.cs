a53bfc0ca4a8d1ab08b9132d5361604b2d6bef3a
Ihor <ihor.dyrman@outlook.com>
2021-06-13 22:07:09 +0300

    Add 876

---
+++ 876/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _876
+{
+    /// <summary>
+    /// 876. Middle of the Linked List
+    /// https://leetcode.com/problems/middle-of-the-linked-list/
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main() { }
+
+        public static ListNode MiddleNode(ListNode head)
+        {
+            if (head == null) return null;
+            var tempNode = head;
+            var count = 0;
+            while (tempNode.next != null)
+            {
+                count++;
+                tempNode = tempNode.next;
+            }
+
+            var middle = count / 2;
+
+            if (count % 2 != 0)
+            {
+                middle++;
+            }
+
+            count = 0;
+            while (count != middle)
+            {
+                count++;
+                head = head.next;
+            }
+
+            return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public readonly int val;
+        public readonly ListNode next;
+
+        protected ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
