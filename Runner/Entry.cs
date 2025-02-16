9d4937cdff436391001a998ae5c78119b3a10c05
ihordyrman <ihor.dyrman@outlook.com>
2025-02-16 19:33:55 +0200

    Invert binary tree

--- Runner/Entry.cs
+++ Runner/Entry.cs
@@ -1,8 +1,18 @@
-﻿namespace Runner;
+﻿using Runner.Easy;
+
+namespace Runner;
 
 public class Entry
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
+        var x = new _226();
+
+        var root = new _226.TreeNode(
+            4,
+            new _226.TreeNode(2, new _226.TreeNode(1), new _226.TreeNode(3)),
+            new _226.TreeNode(7, new _226.TreeNode(6), new _226.TreeNode(9)));
+
+        x.InvertTree(root);
     }
 }
