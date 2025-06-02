6df39d2a88a93813b57814d26fbd852c08133a73
Ihor <ihor.dyrman@outlook.com>
2025-06-02 21:42:15 +0300

    121

--- LeetCode/Problems/Easy/121.cs
+++ LeetCode/Problems/Easy/121.cs
@@ -10,8 +10,10 @@ public class _121
 {
     private static readonly int[] Input = [7, 1, 5, 3, 6, 4];
 
+    public static void Execute() => Console.WriteLine(MaxProfit(Input));
+
     [BenchmarkGen]
-    public void FastMaxProfit() => FastMaxProfit(Input);
+    public void FastMaxProfit() => MaxProfit(Input);
 
     private static int FastMaxProfit(int[] prices)
     {
@@ -34,5 +36,25 @@ public class _121
         }
 
         return maxProfit;
+    }
+
+    // The new attempt after several years
+    private static int MaxProfit(int[] prices)
+    {
+        if (prices is { Length: 0 or 1 }) return 0;
+
+        int minPrice = prices[0];
+        int profit = 0;
+        for (var i = 1; i < prices.Length; i++)
+        {
+            if (minPrice > prices[i])
+            {
+                minPrice = prices[i];
+            }
+
+            profit = Math.Max(profit, prices[i] - minPrice);
+        }
+
+        return profit;
     }
 }
