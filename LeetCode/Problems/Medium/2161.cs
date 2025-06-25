911e4f59abfa6c22b2c9b190991676b85483583a
Ihor <ihor.dyrman@outlook.com>
2025-06-25 18:45:20 +0300

    2161. Partition Array According to Given Pivot

--- LeetCode/Problems/Medium/2161.cs
+++ LeetCode/Problems/Medium/2161.cs
@@ -11,34 +11,9 @@ public class _2161
     private static readonly int[] Input = [9, 12, 5, 10, 14, 3, 10];
 
     // [9,5,3,10,10,12,14]
-    public static void Execute() => PivotArrayBetterOne(Input, 10).Display();
+    public static void Execute() => PivotArray(Input, 10).Display();
 
-    // First attempt. Not optimal
     private static int[] PivotArray(int[] nums, int pivot)
-    {
-        List<int> smaller = [];
-        List<int> larger = [];
-        int pivots = 0;
-
-        foreach (var num in nums)
-        {
-            if (num < pivot)
-                smaller.Add(num);
-            else if (num > pivot)
-                larger.Add(num);
-            else
-                pivots++;
-        }
-
-        for (int i = 0; i < pivots; i++)
-        {
-            smaller.Add(pivot);
-        }
-
-        return smaller.Concat(larger).ToArray();
-    }
-
-    private static int[] PivotArrayBetterOne(int[] nums, int pivot)
     {
         int[] result = new int[nums.Length];
         int pointer = 0;
