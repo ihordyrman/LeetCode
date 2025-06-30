8666c5c1d4bece14d3b9b53b4ff2e10575595bfa
Ihor <ihor.dyrman@outlook.com>
2025-06-30 16:41:31 +0300

    2657. First attempt

---
+++ LeetCode/Problems/Medium/2657.cs
@@ -0,0 +1,36 @@
+using LeetCode.Extensions;
+
+namespace LeetCode.Problems.Medium;
+
+/// <summary>
+///     2657. Find the Prefix Common Array of Two Arrays
+///     https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays
+/// </summary>
+public class _2657
+{
+    private static readonly int[] Input1 = [1, 3, 2, 4];
+    private static readonly int[] Input2 = [3, 1, 2, 4];
+    private static readonly int[] Input3 = [2, 3, 1];
+    private static readonly int[] Input4 = [3, 1, 2];
+
+    public static void Execute()
+    {
+        FindThePrefixCommonArrayLazyAttempt(Input1, Input2).Display();
+        FindThePrefixCommonArrayLazyAttempt(Input3, Input4).Display();
+    }
+
+    private static int[] FindThePrefixCommonArrayLazyAttempt(int[] A, int[] B)
+    {
+        int[] result = new int[A.Length];
+        Dictionary<int, int> counts = [];
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (!counts.TryAdd(A[i], 1)) counts[A[i]]++;
+            if (!counts.TryAdd(B[i], 1)) counts[B[i]]++;
+            result[i] = counts.Values.Count(x => x == 2);
+        }
+
+        return result;
+    }
+}
