c8a5dc42bc890f82051af044ef839e8bc500da07
Ihor <ihor.dyrman@outlook.com>
2025-07-05 20:52:20 +0300

    1394. Find Lucky Integer in an Array

---
+++ LeetCode/Problems/Easy/1394.cs
@@ -0,0 +1,31 @@
+namespace LeetCode.Problems.Easy;
+
+/// <summary>
+///     1394. Find Lucky Integer in an Array
+///     https://leetcode.com/problems/find-lucky-integer-in-an-array
+/// </summary>
+public class _1394
+{
+    private static readonly int[] Input = [2, 2, 3, 4];
+
+    public static void Execute() => Console.WriteLine(FindLucky(Input));
+
+    private static int FindLucky(int[] arr)
+    {
+        Dictionary<int, int> dic = [];
+
+        foreach (int i in arr)
+        {
+            if (!dic.TryAdd(i, 1)) dic[i]++;
+        }
+
+        int maxLucky = -1;
+        foreach (var (key, value) in dic)
+        {
+            if (key == value && value > maxLucky)
+                maxLucky = value;
+        }
+
+        return maxLucky;
+    }
+}
