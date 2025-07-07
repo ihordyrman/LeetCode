46e886c2ca3e59e7ff54e1f823f96ea2d94e4b85
Ihor <ihor.dyrman@outlook.com>
2025-07-07 21:14:11 +0300

    1980. Find Unique Binary String

--- LeetCode/Problems/Medium/1980.cs
+++ LeetCode/Problems/Medium/1980.cs
@@ -1,7 +1,12 @@
+using System.Text;
 using LeetCode.Extensions;
 
 namespace LeetCode.Problems.Medium;
 
+/// <summary>
+///     1980. Find Unique Binary String
+///     https://leetcode.com/problems/find-unique-binary-string
+/// </summary>
 public class _1980
 {
     private static readonly string[] Input = ["01", "10"]; // 11
@@ -11,10 +16,10 @@ public class _1980
 
     public static void Execute()
     {
-        Console.WriteLine($"Input {Input.Stringify()}, Output: {FindDifferentBinaryString(Input)}");
-        Console.WriteLine($"Input {Input2.Stringify()}, Output: {FindDifferentBinaryString(Input2)}");
-        Console.WriteLine($"Input {Input3.Stringify()}, Output: {FindDifferentBinaryString(Input3)}");
-        Console.WriteLine($"Input {Input4.Stringify()}, Output: {FindDifferentBinaryString(Input4)}");
+        Console.WriteLine($"Input {Input.Stringify()}, Output: {FindDifferentBinaryStringBetter(Input)}");
+        Console.WriteLine($"Input {Input2.Stringify()}, Output: {FindDifferentBinaryStringBetter(Input2)}");
+        Console.WriteLine($"Input {Input3.Stringify()}, Output: {FindDifferentBinaryStringBetter(Input3)}");
+        Console.WriteLine($"Input {Input4.Stringify()}, Output: {FindDifferentBinaryStringBetter(Input4)}");
     }
 
     // First attempt. Not great, not terrible.
@@ -36,7 +41,7 @@ public class _1980
 
         for (int i = min + 1; i < max; i++)
         {
-            if (!numbers.Contains(i))
+            if (!numbers.Add(i))
             {
                 return Convert.ToString(i, 2).PadLeft(length, '0');
             }
@@ -45,5 +50,20 @@ public class _1980
         var result = new string('0', length);
         if (nums.Contains(result)) return new string('1', length);
         return result;
+    }
+
+    // Since we have a matrix
+    //  n == nums.length
+    //  nums[i].length == n
+    private static string FindDifferentBinaryStringBetter(string[] nums)
+    {
+        StringBuilder result = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result.Append(nums[i][i] == '0' ? '1' : '0');
+        }
+
+        return result.ToString();
     }
 }
